fc10e71d3fc085a53cd3ac3cd24bd43e9141c886
gabao55 <gabriel.s.rosin@gmail.com>
2022-12-20 18:37:17 -0300

    finishes simplified version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Linq;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 namespace DistanciaCidades
 {
@@ -10,29 +15,72 @@ namespace DistanciaCidades
             string distancesFileName = "matriz.txt";
 
             string distancesFileAddress = Path.Combine(desktopPath, distancesFileName);
+
+            string matrixText = "00,15,30,05,12\n15,00,10,17,28\n30,10,00,03,11\n05,17,03,00,80\n12,28,11,80,00";
+            File.WriteAllText(distancesFileAddress, matrixText);
             
-            string[] distancesLines = File.ReadAllLines(distancesFileAddress);
+            int[,] distancesArray = new int[5, 5];
 
-            int[,] distancesArray = new int[distancesLines.Length, distancesLines.Length];
-
-            for (int i = 0; i < distancesLines.Length; i++)
-            { 
-                int[] distances = distancesLines[i].Split(",").Select(n => Convert.ToInt32(n)).ToArray();
-                distances = distances.ToArray();
-
-                for (int j = 0; j < distances.Length; j++)
+            using (var reader = new StreamReader(distancesFileAddress)) 
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    distancesArray[i, j] = distances[j];
+                    HasHeaderRecord = false,
+                };
+                using (var csv = new CsvReader(reader, config)) 
+                {
+                    int i = 0;
+                    csv.Read();
+                    var record = csv.GetRecord<DistanceInfo>();
+                    distancesArray[i, 0] = record.First;
+                    distancesArray[i, 1] = record.Second;
+                    distancesArray[i, 2] = record.Third;
+                    distancesArray[i, 3] = record.Fourth;
+                    distancesArray[i, 4] = record.Fifth;
+                    i++;
+                    while (csv.Read())
+                    {
+                        record = csv.GetRecord<DistanceInfo>();
+                        distancesArray[i, 0] = record.First;
+                        distancesArray[i, 1] = record.Second;
+                        distancesArray[i, 2] = record.Third;
+                        distancesArray[i, 3] = record.Fourth;
+                        distancesArray[i, 4] = record.Fifth;
+                        i++;
+                    }
                 }
             }
 
             string pathsFileName = "caminhos.txt";
 
             string pathsFileAddress = Path.Combine(desktopPath, pathsFileName);
-            
-            string[] pathsLine = File.ReadAllLines(pathsFileAddress);
 
-            int[] paths = pathsLine[0].Split(", ").Select(n => Convert.ToInt32(n)).ToArray();
+            string pathsText = "1, 2, 3, 2, 5, 1, 4";
+            File.WriteAllText(pathsFileAddress, pathsText);
+
+            int[] paths = new int[7];
+
+            using (var reader = new StreamReader(pathsFileAddress))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = ", ",
+                    HasHeaderRecord = false,
+                };
+                using (var csv = new CsvReader(reader, config)) 
+                {
+                    int i = 0;
+                    csv.Read();
+                    var record = csv.GetRecord<PathInfo>();
+                    paths[0] = record.First;
+                    paths[1] = record.Second;
+                    paths[2] = record.Third;
+                    paths[3] = record.Fourth;
+                    paths[4] = record.Fifth;
+                    paths[5] = record.Sixth;
+                    paths[6] = record.Seventh;
+                }
+            }
 
             int acc = 0;
 
@@ -45,5 +93,37 @@ namespace DistanciaCidades
 
             Console.WriteLine($"A distância percorrida foi de {acc.ToString()} km");
         }
+    }
+
+    public class DistanceInfo
+    {
+        [Index(0)]
+        public int First { get; set; }
+        [Index(1)]
+        public int Second { get; set; }
+        [Index(2)]
+        public int Third { get; set; }
+        [Index(3)]
+        public int Fourth { get; set; }
+        [Index(4)]
+        public int Fifth { get; set; }
+    }
+
+    public class PathInfo
+    {
+        [Index(0)]
+        public int First { get; set; }
+        [Index(1)]
+        public int Second { get; set; }
+        [Index(2)]
+        public int Third { get; set; }
+        [Index(3)]
+        public int Fourth { get; set; }
+        [Index(4)]
+        public int Fifth { get; set; }
+        [Index(5)]
+        public int Sixth { get; set; }
+        [Index(6)]
+        public int Seventh { get; set; }
     }
 }
