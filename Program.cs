aced182ece1125aab71a37420e6e3d391bd0f0df
gabao55 <gabriel.s.rosin@gmail.com>
2022-12-21 10:44:05 -0300

    makes program more generic for solving different dimensions problems

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace DistanciaCidades
             string matrixText = "00,15,30,05,12\n15,00,10,17,28\n30,10,00,03,11\n05,17,03,00,80\n12,28,11,80,00";
             File.WriteAllText(distancesFileAddress, matrixText);
             
-            int[,] distancesArray = new int[5, 5];
+            int[,] distancesMatrix;
 
             using (var reader = new StreamReader(distancesFileAddress)) 
             {
@@ -27,26 +27,21 @@ namespace DistanciaCidades
                 {
                     HasHeaderRecord = false,
                 };
-                using (var csv = new CsvReader(reader, config)) 
+                using (var csv = new CsvParser(reader, config)) 
                 {
                     int i = 0;
+
                     csv.Read();
-                    var record = csv.GetRecord<DistanceInfo>();
-                    distancesArray[i, 0] = record.First;
-                    distancesArray[i, 1] = record.Second;
-                    distancesArray[i, 2] = record.Third;
-                    distancesArray[i, 3] = record.Fourth;
-                    distancesArray[i, 4] = record.Fifth;
-                    i++;
+                    var record = csv.Record;
+                    distancesMatrix = new int[record.Length, record.Length];
+                    
+                    MatrixAndArrayPopulator.PopulateMatrixRow(distancesMatrix, i, record);
+
                     while (csv.Read())
                     {
-                        record = csv.GetRecord<DistanceInfo>();
-                        distancesArray[i, 0] = record.First;
-                        distancesArray[i, 1] = record.Second;
-                        distancesArray[i, 2] = record.Third;
-                        distancesArray[i, 3] = record.Fourth;
-                        distancesArray[i, 4] = record.Fifth;
                         i++;
+                        record = csv.Record;
+                        MatrixAndArrayPopulator.PopulateMatrixRow(distancesMatrix, i, record);
                     }
                 }
             }
@@ -58,27 +53,21 @@ namespace DistanciaCidades
             string pathsText = "1, 2, 3, 2, 5, 1, 4";
             File.WriteAllText(pathsFileAddress, pathsText);
 
-            int[] paths = new int[7];
+            int[] paths;
 
             using (var reader = new StreamReader(pathsFileAddress))
             {
                 var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    Delimiter = ", ",
+                    Delimiter = ",",
                     HasHeaderRecord = false,
                 };
-                using (var csv = new CsvReader(reader, config)) 
+                using (var csv = new CsvParser(reader, config)) 
                 {
-                    int i = 0;
                     csv.Read();
-                    var record = csv.GetRecord<PathInfo>();
-                    paths[0] = record.First;
-                    paths[1] = record.Second;
-                    paths[2] = record.Third;
-                    paths[3] = record.Fourth;
-                    paths[4] = record.Fifth;
-                    paths[5] = record.Sixth;
-                    paths[6] = record.Seventh;
+                    var record = csv.Record;
+                    paths = new int[record.Length];
+                    MatrixAndArrayPopulator.PopulateArray(paths, record);
                 }
             }
 
@@ -88,42 +77,37 @@ namespace DistanciaCidades
             {
                 int currentLocationIndex = paths[i-1]-1;
                 int nextLocationIndex = paths[i]-1;
-                acc += distancesArray[currentLocationIndex, nextLocationIndex];
+                acc += distancesMatrix[currentLocationIndex, nextLocationIndex];
             }
 
             Console.WriteLine($"A distância percorrida foi de {acc.ToString()} km");
         }
     }
 
-    public class DistanceInfo
+    public static class MatrixAndArrayPopulator
     {
-        [Index(0)]
-        public int First { get; set; }
-        [Index(1)]
-        public int Second { get; set; }
-        [Index(2)]
-        public int Third { get; set; }
-        [Index(3)]
-        public int Fourth { get; set; }
-        [Index(4)]
-        public int Fifth { get; set; }
-    }
+        public static void PopulateMatrixRow(int[,] matrix, int row, string[] rowData)
+        {
+            for (int i = 0; i < rowData.Length; i++)
+            {
+                bool validation = int.TryParse(rowData[i], out int castedData);
+                if (validation)
+                {
+                    matrix[row, i] = castedData;
+                }
+            }
+        }
 
-    public class PathInfo
-    {
-        [Index(0)]
-        public int First { get; set; }
-        [Index(1)]
-        public int Second { get; set; }
-        [Index(2)]
-        public int Third { get; set; }
-        [Index(3)]
-        public int Fourth { get; set; }
-        [Index(4)]
-        public int Fifth { get; set; }
-        [Index(5)]
-        public int Sixth { get; set; }
-        [Index(6)]
-        public int Seventh { get; set; }
+        public static void PopulateArray(int[] array, string[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                bool validation = int.TryParse(data[i], out int castedData);
+                if (validation)
+                {
+                    array[i] = castedData;
+                }
+            }
+        }
     }
 }
